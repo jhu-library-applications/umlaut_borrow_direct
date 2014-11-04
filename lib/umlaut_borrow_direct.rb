6fae0d172bab435df99d359768408a6cbf17f9d3
Jonathan Rochkind <jonathan@dnil.net>
2014-11-04 15:13:10 -0500

    Use standard spinner config for now

--- lib/umlaut_borrow_direct.rb
+++ lib/umlaut_borrow_direct.rb
@@ -7,7 +7,7 @@ module UmlautBorrowDirect
       :div_id     => "borrow_direct",
       :html_area  => :main,
       :partial    => "borrow_direct/resolve_section",
-      :visibility => :responses_exist,
+      #:visibility => :responses_exist,
       :service_type_values => %w{bd_link_to_search bd_request_prompt bd_not_available bd_request_placed}
     }
   end
