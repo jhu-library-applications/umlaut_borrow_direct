c3da10d1ea8e8695ebb0c784318f23d48e793c9e
Jonathan Rochkind <jonathan@dnil.net>
2014-11-06 13:18:17 -0500

    register with umlaut backtrace cleaning whitelist

--- lib/umlaut_borrow_direct/engine.rb
+++ lib/umlaut_borrow_direct/engine.rb
@@ -1,9 +1,16 @@
+require 'umlaut'
+
 module UmlautBorrowDirect
   class Engine < ::Rails::Engine
+    engine_name "umlaut_borrow_direct"
 
     initializer "umlaut_borrow_direct.add_service_types" do |app|
       service_type_hash = Hash[UmlautBorrowDirect.service_type_values.collect {|v| [v, {}] }]
       ServiceTypeValue.merge_hash! service_type_hash
+    end
+
+    initializer "#{engine_name}.backtrace_cleaner", :before => "umlaut.backtrace_cleaner" do
+      Umlaut::Engine.config.whitelisted_backtrace[self.root] = self.engine_name
     end
 
   end
