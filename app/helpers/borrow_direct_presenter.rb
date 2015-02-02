93dece4779e0baa8e5bb82b55022f0a3a6b6e9f3
Jonathan Rochkind <jonathan@dnil.net>
2015-02-02 18:08:46 -0500

    more UI tweaking

--- app/helpers/borrow_direct_presenter.rb
+++ app/helpers/borrow_direct_presenter.rb
@@ -79,7 +79,7 @@ class BorrowDirectPresenter
   end
 
   def show_link_response?
-    self.link_response
+    self.link_response && ! self.show_confirmation?
   end
 
   def could_not_place_request?
